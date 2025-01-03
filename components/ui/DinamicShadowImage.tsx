ca85d7cf2ed1791f14b2860871b7ea8619e17f81
Itta Funahashi <itta@itta.dev>
2025-01-03 14:41:20 +0900

    feat: update profile icon

--- components/ui/DinamicShadowImage.tsx
+++ components/ui/DinamicShadowImage.tsx
@@ -1,24 +1,35 @@
-import { Box } from '@chakra-ui/react';
-import Image from 'next/image';
-import type { ImageProps } from 'next/image';
-import { FC } from 'react';
+import { Box } from "@chakra-ui/react";
+import Image from "next/image";
+import type { ImageProps } from "next/image";
+import { FC } from "react";
 
-const DinamicShadowImage: FC<ImageProps> = ({ width, height, alt, ...props }) => {
+const DinamicShadowImage: FC<ImageProps> = ({
+  width,
+  height,
+  alt,
+  ...props
+}) => {
   return (
     <Box position="relative" width={width} height={height}>
       <Box
         position="absolute"
         inset={0}
-        filter="blur(8px)"
-        transform="scale(1.1, 1.1)"
+        filter="blur(10px)"
+        transform="scale(1.05, 1.05)"
         userSelect="none"
         pointerEvents="none"
       >
-        <Box overflow="hidden" rounded="xl">
+        <Box overflow="hidden" rounded="2xl">
           <Image width={width} height={height} alt={alt} {...props} />
         </Box>
       </Box>
-      <Box rounded="lg" position="relative" inset={0} overflow="hidden">
+      <Box
+        rounded="2xl"
+        position="relative"
+        inset={0}
+        overflow="hidden"
+        shadow="lg"
+      >
         <Image width={width} height={height} alt={alt} {...props} />
       </Box>
     </Box>
